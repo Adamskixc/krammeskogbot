947c72d6d7069fd99bf08c9ced25515cadb81933
Adamskixc <48383577+Adamskixc@users.noreply.github.com>
2020-05-13 13:11:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const Sentry = require('@sentry/node')
+Sentry.init({ dsn: process.env.SENTRY_DSN })
+
+const { readFileSync } = require('fs')
+
+require('moment')
+require('moment-duration-format')
+
+// Initialize Canvas
+let canvasLoaded = false
+try {
+  require('canvas')
+  require('./src/utils/CanvasUtils.js').initializeHelpers()
+  canvasLoaded = true
+} catch (e) {}
+
+// Initialize client
+const CLIENT_OPTIONS = {
+  fetchAllMembers: true,
+  enableEveryone: false,
+  canvasLoaded
+}
+
+console.log(readFileSync('bigtitle.txt', 'utf8').toString())
+
+const Switchblade = require('./src/Switchblade.js')
+const client = new Switchblade(CLIENT_OPTIONS, Sentry)
+client.login().then(() => client.log('Logged in successfully!', { color: 'green', tags: ['Discord'] }))
